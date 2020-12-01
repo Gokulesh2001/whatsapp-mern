cb31c9768e66317bab267da90d5b3e2c08c7c51d
my-name <kagokulesh@gmail.com>
2020-12-01 21:53:46 +0530

    Second Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNS7k6cwJaAX3ttIweS2D7eFr-2QIVY64",
+    authDomain: "whatsapp-mern-64a46.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-64a46.firebaseio.com",
+    projectId: "whatsapp-mern-64a46",
+    storageBucket: "whatsapp-mern-64a46.appspot.com",
+    messagingSenderId: "1004550970311",
+    appId: "1:1004550970311:web:98cb6dce010337fdecd7e2"
+  };
