7f975bbed447ce85abf238c6f7ac1e5d25676a99
my-name <kagokulesh@gmail.com>
2020-12-02 21:28:14 +0530

    3rd update

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
 function Sidebar() {
     return (
-        <div>
-           <h1>Sidebar</h1>  
+        <div className="sidebar">  
+           <div className="sidebar__header">
+           <Avatar src="https://media-exp1.licdn.com/dms/image/C5603AQH7DC6GgzVn0g/profile-displayphoto-shrink_200_200/0/1597904535705?e=1612396800&v=beta&t=73n30ZX1QP_CIQiduPHvQkTXHZPookw7tX3bmPiMM-M"/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                    <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                    <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                    <MoreVertIcon />
+                    </IconButton>
+
+                </div>
+           </div>
+           <div className="sidebar__search">
+             <div className="sidebar__searchContainer">
+                <SearchOutlined />
+                <input placeholder="Search or start new chat" type="text" />
+             </div>
+           </div>
         </div>
     )
 }
